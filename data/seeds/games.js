6d5f4a7acd4e8018db090fa32b46bc9726a7bbc0
Alan Perez <alanp9six@gmail.com>
2019-05-17 14:41:08 -0500

    fixed error

--- data/seeds/games.js
+++ data/seeds/games.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('games').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('games').insert([
+        {title: "Counter-Strike:Source", genre: 'FPS SHOOTER', releaseYear: 2004},
+        {title: "Counter-Strike 1.6", genre: 'FPS SHOOTER', releaseYear: 1996},
+        {title: "Counter-Strike:Global Offensive", genre: 'FPS SHOOTER', releaseYear: 2012}
       ]);
     });
 };
