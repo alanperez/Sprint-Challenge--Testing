dbbceaffd2891e469964c3141dcae3ae552010ca
Alan Perez <alanp9six@gmail.com>
2019-05-17 15:06:07 -0500

    mvp!

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-
-const server = require('./api/server/js');
+// require('dotenv').config();
+const server = require('./api/server.js');
 
 const port = process.env.PORT || 3300;
 server.listen(port, () => console.log(`\n** server up on port ${port} **\n`));
